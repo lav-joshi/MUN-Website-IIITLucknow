5e9623633ad91c0a0dfeeba2ef008bad55361cd2
lav-joshi <lcs2019022@iiitl.ac.in>
2020-05-11 14:53:25 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import { MDBBtn, MDBCol, MDBContainer, MDBRow } from "mdbreact";
+import "./index.css";
+import Navbar from './components/NavBar';
+class App extends Component {
+  render() {
+    return (
+      <Navbar/>
+    );
+  }
+}
+
+export default App;
